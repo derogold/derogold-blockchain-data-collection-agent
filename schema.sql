5541eea5f92df0f02568ff917b6c4e54f8bb279d
Brandon Lehmann <brandonlehmann@gmail.com>
2018-11-10 10:15:10 -0500

    Added current schema for the SQL tables

---
+++ schema.sql
@@ -0,0 +1,60 @@
+CREATE TABLE `blocks` (
+  `hash` varchar(64) NOT NULL,
+  `prevHash` varchar(64) NOT NULL,
+  `height` bigint(20) unsigned NOT NULL,
+  `baseReward` bigint(20) unsigned NOT NULL,
+  `difficulty` bigint(20) unsigned NOT NULL,
+  `majorVersion` int(10) unsigned NOT NULL,
+  `minorVersion` int(10) unsigned NOT NULL,
+  `nonce` bigint(20) NOT NULL,
+  `size` int(10) unsigned NOT NULL,
+  `timestamp` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`hash`),
+  KEY `height` (`height`),
+  KEY `timestamp` (`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+ PARTITION BY KEY (`hash`)
+PARTITIONS 100;
+
+CREATE TABLE `transaction_inputs` (
+  `txnHash` varchar(64) NOT NULL,
+  `keyImage` varchar(64) NOT NULL,
+  `amount` bigint(20) unsigned NOT NULL,
+  `type` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`txnHash`,`keyImage`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+ PARTITION BY KEY ()
+PARTITIONS 100;
+
+CREATE TABLE `transaction_outputs` (
+  `txnHash` varchar(64) NOT NULL,
+  `outputIndex` int(10) unsigned NOT NULL,
+  `globalIndex` bigint(20) unsigned NOT NULL,
+  `amount` bigint(20) unsigned NOT NULL,
+  `key` varchar(64) NOT NULL,
+  `type` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`globalIndex`,`amount`),
+  KEY `composite` (`txnHash`,`outputIndex`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+ PARTITION BY KEY ()
+PARTITIONS 100;
+
+CREATE TABLE `transactions` (
+  `txnHash` varchar(64) NOT NULL,
+  `blockHash` varchar(64) NOT NULL,
+  `mixin` int(10) unsigned NOT NULL,
+  `timestamp` bigint(20) unsigned NOT NULL,
+  `paymentId` varchar(64) DEFAULT NULL,
+  `unlockTime` bigint(20) unsigned NOT NULL,
+  `publicKey` varchar(64) NOT NULL,
+  `fee` bigint(20) unsigned NOT NULL,
+  `size` int(10) unsigned NOT NULL,
+  `nonce` varchar(255) DEFAULT NULL,
+  `extra` blob DEFAULT NULL,
+  PRIMARY KEY (`txnHash`),
+  KEY `blockHash` (`blockHash`),
+  KEY `paymentId` (`paymentId`),
+  KEY `timestamp` (`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+ PARTITION BY KEY (`txnHash`)
+PARTITIONS 100;
