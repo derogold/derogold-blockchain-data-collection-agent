115ec9a2d9c8bdff3df726ab9dd26fcd0c98d42f
Brandon Lehmann <brandonlehmann@gmail.com>
2018-11-11 23:02:30 -0500

    The whole package needed a massive overhaul to make sure that we are handling chain forks correctly. This update changes the package to sync the cache more like how the current wallets talk to the daemon for their internal state.
    This update does require the use of https://github.com/turtlecoin/turtlecoin/pull/606 that adds support for the method that we are using to perform the sync.

--- index.js
+++ index.js
@@ -5,31 +5,23 @@
 'use strict'
 
 const Config = require('./config.json')
-const BlockChainMonitor = require('./lib/blockchainMonitor')
 const BlockChainCollector = require('./lib/blockchainCollector')
 const DatabaseBackend = require('./lib/databaseBackend')
 const Metronome = require('./lib/metronome')
 const util = require('util')
 
-const monitor = new BlockChainMonitor({
-  host: Config.node.host,
-  port: Config.node.port
-})
+/* Let's set up a standard logger. Sure it looks cheap but it's
+   reliable and won't crash */
+function log (message) {
+  console.log(util.format('%s: %s', (new Date()).toUTCString(), message))
+}
 
-/* Set up our blockchain collector so that we can actually query
-   the daemon and go get block details. The timeout is adjusted
-   dynamically based on the amount of blocks we're going to try
-   to get at one time. This is because the more blocks we request,
-   the longer the daemon takes to respond with those blocks. It's
-   not a linear relation by any means but the method below seems
-   to provide at least a semi-reliable way to figure out the timeout
-   we need to use */
-const collector = new BlockChainCollector({
-  host: Config.node.host,
-  port: Config.node.port,
-  timeout: ((Config.catchUpBlockIncrement * 500) >= 5000) ? Config.catchUpBlockIncrement * 500 : 5000
-})
+/* We're going to go ahead and create our timer but pause it until
+   we know that we have the genesis hash in the database */
+const timer = new Metronome(2000)
+timer.pause = true
 
+/* Set up our database connection */
 const database = new DatabaseBackend({
   host: Config.mysql.host,
   port: Config.mysql.port,
@@ -39,26 +31,14 @@ const database = new DatabaseBackend({
   connectionLimit: Config.mysql.connectionLimit
 })
 
-function log (message) {
-  console.log(util.format('%s: %s', (new Date()).toUTCString(), message))
-}
-
-monitor.on('update', (block) => {
-  /* Every time the blockchain monitor tells us there's a new top block,
-     let's go out and collect it */
-  collectBlocks(block.height).then(() => {
-    log('Collected block: ' + block.height)
-  }).catch(() => {
-    log('Could not collect block: ' + block.height)
-  })
-})
-
-monitor.on('fork', () => {
-  // fork?
-})
-
-monitor.on('error', () => {
-  // for now we are suppressing errors because we'll just try again later
+/* Set up our blockchain collector so that we can actually query
+   the daemon and go get block details. The timeout is set
+   statically to 60s for the time being so that we can monitor
+   performance as it plays catch up */
+const collector = new BlockChainCollector({
+  host: Config.node.host,
+  port: Config.node.port,
+  timeout: 60000
 })
 
 database.haveGenesis().then((haveGenesis) => {
@@ -67,115 +47,40 @@ database.haveGenesis().then((haveGenesis) => {
      could not collect the genesis block, we've got big problems */
   if (haveGenesis) {
     log('Genesis block found in database')
+    timer.pause = false
   } else {
-    collectBlocks(0).then(() => {
-      log('Collected block: 0')
-    }).catch(() => {
-      log('Could not collect genesis block')
+    collector.getGenesis().then((genesis) => {
+      return database.saveBlock(genesis)
+    }).then(() => {
+      log('Collected genesis block')
+      timer.pause = false
+    }).catch((error) => {
+      log('Could not collect genesis block: ' + error)
     })
   }
 }).catch(() => {
   log('Could not check for genesis block in database')
 })
 
-const catchupTimer = new Metronome(Config.catchUpInterval)
-
-catchupTimer.on('tick', () => {
-  /* Every tick, check to see if we're missing any blocks in the
-     database, if we are, we have some work to do */
-  database.detectMissingBlocks().then(async (results) => {
-    if (results.length > 0) {
-      /* Pause the timer as we don't want to overrun ourselves later
-         and overload the daemon as it does NOT care for that at all... */
-      catchupTimer.pause = true
-      const result = results[0]
-      var increment = Config.catchUpBlockIncrement
-      var passCount = 0
-
-      var min = result.lowerBound
-      var max = result.upperBound
-      var done = false
-      var lastCallSuccess = true
-
-      do {
-        /* If our last attempt to get collect blocks failed its likely
-           due to hitting the RPC timeout -- the only way to handle this
-           is to back off a bit and request half the blocks we did on the lass
-           pass. If we keep failing, we'll step all the way back down to 1
-           block at a time if we have to */
-        if (!lastCallSuccess) {
-          increment = Math.floor(increment / 2)
-        }
-
-        if (increment < 1) increment = 1
-        var diff = max - min
-
-        /* Sometimes weird things happen. Weird things need accounted for */
-        max = (diff > increment) ? min + increment : max
-        if (max < min) max = min
-        if (max === min) max++
-
-        log('Requesting blocks: ' + min + ' -> ' + max + ' (' + (max - min) + ')')
-        try {
-          var collected = await collectBlocks(min, max)
-
-          // Awesome, we collected some blocks
-          if (collected.min && collected.max) {
-            log('Collected blocks: ' + min + ' -> ' + max + ' (' + (max - min) + ')')
-            if (max === result.upperBound) done = true
-
-            /* Here's where we increase where we're currently at in the missing
-               blocks range */
-            min = collected.max
-            max = result.upperBound
-            lastCallSuccess = true
-            passCount++
-
-            /* If we've been able to collect blocks at a decreased count value
-               for the last 10 passes, let's try increasing how many we're grabbing
-               so that we can work our way back up to the value specified in our config */
-            if (passCount > 10 && increment < Config.catchUpBlockIncrement) {
-              increment = increment * 2
-              if (increment > Config.catchUpBlockIncrement) increment = Config.catchUpBlockIncrement
-            }
-          }
-        } catch (err) {
-          passCount = 0
-          lastCallSuccess = false
-        }
-      } while (!done)
-
-      catchupTimer.pause = false
+timer.on('tick', () => {
+  timer.pause = true // let's not tick again we're ready
+  database.getLastKnownBlockHashes().then((lastKnownHashes) => {
+    return collector.queryBlocks(lastKnownHashes)
+  }).then((results) => {
+    return database.saveBlocks(results.blocks, results.height)
+  }).then((results) => {
+    for (var i = 0; i < results.blocks.length; i++) {
+      log('Saved block #' + results.blocks[i].height + ' (' + results.blocks[i].hash + ')')
     }
+    log('============   ' + results.deletes + ' Delete Statements  ============')
+    log('============   Stored ' + results.inserts + ' Objects   ============')
+
+    /* Allow our timer to fire again */
+    timer.pause = false
   }).catch((error) => {
-    catchupTimer.pause = false
     log(error)
+
+    /* Allow our timer to fire again */
+    timer.pause = false
   })
 })
-
-function collectBlocks (min, max) {
-  return new Promise((resolve, reject) => {
-    /* Try to retrieve all of the block details from the daemon
-       for the blocks between min and max */
-    collector.get(min, max).then((blocks) => {
-      if (blocks.blocks && blocks.blocks.length >= 1) {
-        var promises = []
-        for (var i = 0; i < blocks.blocks.length; i++) {
-          promises.push(database.saveBlock(blocks.blocks[i]))
-        }
-
-        /* Wait to see if we were able to save all of the block details
-           that we collected */
-        Promise.all(promises).then(() => {
-          if (typeof max === 'undefined') {
-            return resolve({ min, max: min })
-          } else {
-            return resolve({ min, max })
-          }
-        })
-      }
-    }).catch((error) => {
-      return reject(error)
-    })
-  })
-}
--- schema.sql
+++ schema.sql
@@ -9,11 +9,12 @@ CREATE TABLE `blocks` (
   `nonce` bigint(20) NOT NULL,
   `size` int(10) unsigned NOT NULL,
   `timestamp` bigint(20) unsigned NOT NULL,
-  PRIMARY KEY (`hash`),
-  KEY `height` (`height`),
-  KEY `timestamp` (`timestamp`)
+  PRIMARY KEY (`height`),
+  KEY `timestamp` (`timestamp`),
+  KEY `prevHash` (`prevHash`),
+  KEY `hash` (`hash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
- PARTITION BY KEY (`hash`)
+ PARTITION BY KEY (`height`)
 PARTITIONS 100;
 
 CREATE TABLE `transaction_inputs` (
@@ -33,8 +34,8 @@ CREATE TABLE `transaction_outputs` (
   `amount` bigint(20) unsigned NOT NULL,
   `key` varchar(64) NOT NULL,
   `type` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`globalIndex`,`amount`),
-  KEY `composite` (`txnHash`,`outputIndex`),
+  PRIMARY KEY (`txnHash`,`outputIndex`),
+  KEY `globalIndex` (`globalIndex`,`amount`),
   KEY `amount` (`amount`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
  PARTITION BY KEY ()
@@ -54,10 +55,8 @@ CREATE TABLE `transactions` (
   `extra` blob DEFAULT NULL,
   PRIMARY KEY (`txnHash`),
   KEY `blockHash` (`blockHash`),
-  KEY `paymentId` (`paymentId`),
   KEY `timestamp` (`timestamp`),
-  KEY `publicKey` (`publicKey`)
+  KEY `paymentId` (`paymentId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
  PARTITION BY KEY (`txnHash`)
 PARTITIONS 100;
-
