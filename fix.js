1ef5cc823ca44a1ee4447e632a7c6d635139b8d9
Brandon Lehmann <brandonlehmann@gmail.com>
2018-11-29 18:16:17 -0500

    Adding a script that allows us to information from an arbitrary start height to resolve issues with database corruption as a result of prior issues before the last commit

---
+++ fix.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2018, TurtlePay Developers
+//
+// Please see the included LICENSE file for more information.
+
+'use strict'
+
+const badBlockStart = 1015250
+const DatabaseBackend = require('./lib/databaseBackend')
+const Config = require('./config.json')
+
+const database = new DatabaseBackend({
+  host: Config.mysql.host,
+  port: Config.mysql.port,
+  username: Config.mysql.username,
+  password: Config.mysql.password,
+  database: Config.mysql.database,
+  connectionLimit: Config.mysql.connectionLimit
+})
+
+database.buildDeleteBlocksFromHeightQueries(badBlockStart).then((queries) => {
+  return database._insertTransaction(queries)
+}).then((results) => {
+  console.log(results)
+}).catch((error) => {
+  console.log(error)
+})
