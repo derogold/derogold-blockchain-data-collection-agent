a4bc350cd02dbbf2806e68f2f823231f2846ae0a
Brandon Lehmann <brandonlehmann@gmail.com>
2019-09-13 15:22:05 -0400

    Pack all the things

--- schema.sql
+++ schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE `blocks` (
   KEY `prevHash` (`prevHash`),
   KEY `hash` (`hash`),
   KEY `transactionCount` (`transactionCount`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPRESSED;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 PACK_KEYS=1 ROW_FORMAT=COMPRESSED;
 
 CREATE TABLE `transaction_inputs` (
   `txnHash` varchar(64) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE `transaction_inputs` (
   `amount` bigint(20) unsigned NOT NULL,
   `type` int(10) unsigned NOT NULL,
   PRIMARY KEY (`txnHash`,`keyImage`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPRESSED;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 PACK_KEYS=1 ROW_FORMAT=COMPRESSED;
 
 CREATE TABLE `transaction_outputs` (
   `txnHash` varchar(64) NOT NULL,
@@ -41,7 +41,7 @@ CREATE TABLE `transaction_outputs` (
   PRIMARY KEY (`txnHash`,`outputIndex`),
   KEY `globalIndex` (`globalIndex`,`amount`),
   KEY `amount` (`amount`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPRESSED;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 PACK_KEYS=1 ROW_FORMAT=COMPRESSED;
 
 CREATE TABLE `transactions` (
   `txnHash` varchar(64) NOT NULL,
@@ -62,7 +62,7 @@ CREATE TABLE `transactions` (
   KEY `blockHash` (`blockHash`),
   KEY `timestamp` (`timestamp`),
   KEY `paymentId` (`paymentId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPRESSED;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 PACK_KEYS=1 ROW_FORMAT=COMPRESSED;
 
 CREATE TABLE `transaction_pool` (
   `txnHash` varchar(64) NOT NULL,
@@ -73,19 +73,19 @@ CREATE TABLE `transaction_pool` (
   KEY `fee` (`fee`),
   KEY `amount` (`amount`),
   KEY `size` (`size`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPRESSED;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 PACK_KEYS=1 ROW_FORMAT=COMPRESSED;
 
 CREATE TABLE `transaction_outputs_index_maximums` (
   `amount` bigint(20) NOT NULL,
   `globalIndex` bigint(20) NOT NULL,
   PRIMARY KEY (`amount`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPRESSED;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 PACK_KEYS=1 ROW_FORMAT=COMPRESSED;
 
 CREATE TABLE `information` (
   `key` varchar(255) NOT NULL,
   `payload` blob NOT NULL,
   PRIMARY KEY (`key`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPRESSED;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 PACK_KEYS=1 ROW_FORMAT=COMPRESSED;
 
 DROP TRIGGER IF EXISTS `transaction_outputs_AFTER_INSERT`;
 
